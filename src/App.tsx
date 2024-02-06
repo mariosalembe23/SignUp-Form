110e500c86d648b5a39c8796311372e62156c8bb
Mário Lino Salembe <linomario199010@gmail.com>
2024-02-06 10:44:55 +0100

    Adicionados Inputs Group

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,50 @@ function App() {
         <div className="h-96 bg-[url('/img/patterns.svg')] rounded-l-lg back_patterns"></div>
         <div className="p-5 bg-[#fff] rounded-r-md">
           <header className="text-center flex flex-col justify-center">
-            <a href="#" className="font-normal text-xl text-zinc-500">
+            <a href="#" className=" text-xl text-indigo-400 font-medium">
               Morphosis
             </a>
-            <small className="text-zinc-400">Focados em Proteger seus Dados</small>
+            <small className="text-zinc-400">
+              Focados em Proteger seus Dados
+            </small>
           </header>
+
+          <div className="form_container mt-8">
+            <form className="w-full">
+              <div className="container_inputs grid grid-cols-1 gap-5">
+                <div className="input_group space-y-3 flex flex-col">
+                  <label
+                    htmlFor="email"
+                    className="text-zinc-500 font-ligt font-['Inter'] px-3"
+                  >
+                    E-mail
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="w-full px-3 py-2.5 outline-none transition-all focus:ring-2 ring-zinc-300 rounded bg-gray-100 placeholder:text-zinc-500 "
+                    placeholder="mario@gmail.com"
+                  />
+                </div>
+                <div className="input_group space-y-3 flex flex-col">
+                  <label
+                    htmlFor="password"
+                    className="text-zinc-500 font-ligt font-['Inter'] px-3"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    className="w-full px-3 py-2.5 outline-none transition-all focus:ring-2 ring-zinc-300 rounded bg-gray-100 placeholder:text-zinc-500 "
+                    placeholder="*********"
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </main>
